dcc6a7a6b452a4a9e6b9c76ec72b177acc93c674
Freekiehsoes <freekattema@live.nl>
2023-10-09 13:51:59 +0200

    update

--- cursor.go
+++ cursor.go
@@ -2,52 +2,45 @@ package cursor
 
 import "fmt"
 
-type Cursor struct {
-}
-
-func (c *Cursor) Up(n int) {
+func Up(n int) {
 	fmt.Printf("\033[%dA", n)
 }
-func (c *Cursor) Down(n int) {
+func Down(n int) {
 	fmt.Printf("\033[%dB", n)
 }
-func (c *Cursor) Right(n int) {
+func Right(n int) {
 	fmt.Printf("\033[%dC", n)
 }
-func (c *Cursor) Left(n int) {
+func Left(n int) {
 	fmt.Printf("\033[%dD", n)
 }
-func (c *Cursor) Top() {
+func Top() {
 	fmt.Printf("\033[%dH", 0)
 }
-func (c *Cursor) Bottom() {
+func Bottom() {
 	fmt.Printf("\033[%dH", 9999)
 }
-func (c *Cursor) LineStart() {
+func LineStart() {
 	fmt.Printf("\033[%dG", 0)
 }
-func (c *Cursor) LineEnd() {
+func LineEnd() {
 	fmt.Printf("\033[%dG", 9999)
 }
-func (c *Cursor) ClearLine() {
+func ClearLine() {
 	fmt.Printf("\033[2K\r")
 }
-func (c *Cursor) ClearScreen() {
+func ClearScreen() {
 	fmt.Printf("\033[2J")
 }
-func (c *Cursor) SavePosition() {
+func SavePosition() {
 	fmt.Printf("\033[s")
 }
-func (c *Cursor) RestorePosition() {
+func RestorePosition() {
 	fmt.Printf("\033[u")
 }
-func (c *Cursor) Hide() {
+func Hide() {
 	fmt.Printf("\033[?25l")
 }
-func (c *Cursor) Show() {
+func Show() {
 	fmt.Printf("\033[?25h")
-}
-
-func GetCursor() *Cursor {
-	return &Cursor{}
 }
