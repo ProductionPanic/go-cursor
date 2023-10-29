79cf03e78795787114148f17ae3fd994a81b6a9c
Freek-ZIGZAG <freek@bureauzigzag.nl>
2023-10-29 03:01:41 +0100

    changes

--- cursor.go
+++ cursor.go
@@ -1,6 +1,10 @@
 package cursor
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func Up(n int) {
 	fmt.Printf("\033[%dA", n)
@@ -43,4 +47,77 @@ func Hide() {
 }
 func Show() {
 	fmt.Printf("\033[?25h")
+}
+func Exec(cmd string) {
+	lines := strings.Split(cmd, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		parts := strings.Split(line, " ")
+		hasArg := len(parts) > 1
+		var arg string
+		if hasArg {
+			arg = parts[1]
+		}
+		switch parts[0] {
+		case "up":
+			if hasArg {
+				argInt, err := strconv.Atoi(arg)
+				if err == nil {
+					Up(argInt)
+				}
+			} else {
+				Up(1)
+			}
+		case "down":
+			if hasArg {
+				argInt, err := strconv.Atoi(arg)
+				if err == nil {
+					Down(argInt)
+				}
+			} else {
+				Down(1)
+			}
+		case "left":
+			if hasArg {
+				argInt, err := strconv.Atoi(arg)
+				if err == nil {
+					Left(argInt)
+				}
+			} else {
+				Left(1)
+			}
+		case "right":
+			if hasArg {
+				argInt, err := strconv.Atoi(arg)
+				if err == nil {
+					Right(argInt)
+				}
+			} else {
+				Right(1)
+			}
+		case "top":
+			Top()
+		case "bottom":
+			Bottom()
+		case "line-start":
+			LineStart()
+		case "line-end":
+			LineEnd()
+		case "clear-line":
+			ClearLine()
+		case "clear-screen":
+			ClearScreen()
+		case "save-position":
+			SavePosition()
+		case "restore-position":
+			RestorePosition()
+		case "hide":
+			Hide()
+		case "show":
+			Show()
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/ProductionPanic/go-cursor
 
-go 1.21.0
+go 1.23
